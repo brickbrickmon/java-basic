599f32c8e3736bfa0c7ae3afd0cd581fac92671b
sub <subhw0704@gmail.com>
2026-07-30 10:04:57 +0900

    sample

---
+++ src/class1/Class1.java
@@ -0,0 +1,12 @@
+package class1;
+
+import static java.lang.IO.print;
+
+public class Class1 {
+
+    static void main(String[] args) {
+
+        // TODO 샘플용
+        print("todo print");
+    }
+}
---
+++ src/class1/Class2.java
@@ -0,0 +1,12 @@
+package class1;
+
+import static java.lang.IO.print;
+
+public class Class2 {
+
+    static void main(String[] args) {
+
+        // TODO 샘플용2
+        print("todo print2");
+    }
+}
